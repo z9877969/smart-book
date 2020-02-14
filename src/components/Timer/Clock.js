95f7058f6934db303a518ae4568c27b3c0567f59
z9877969 <46637279+z9877969@users.noreply.github.com>
2020-02-14 19:05:06 +0200

    add set date from store + slowly transition timer from positive to negative (#3)

--- src/components/Timer/Clock.js
+++ src/components/Timer/Clock.js
@@ -1,53 +1,54 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getTimeTrainingAfterEndObj } from './timerHelpers';
 import css from './Timer.module.css';
 
 const zeroPad = value => (value < 10 ? `0${value}` : value);
 
-const Clock = ({ title, dateOfEndISO, finished }) => {
-  const [date, setDate] = useState(getTimeTrainingAfterEndObj(dateOfEndISO));
+const Clock = ({ finished }) => {
+  const timeEndState = useSelector(state => state.training.timeEnd);
+  const [date, setDate] = useState(getTimeTrainingAfterEndObj(timeEndState));
 
   useEffect(() => {
     if (!finished) {
       setInterval(() => {
-        setDate(getTimeTrainingAfterEndObj(dateOfEndISO));
+        setDate(getTimeTrainingAfterEndObj(timeEndState));
       }, 1000);
     }
   }, []);
 
   return (
-    <div className={css.container}>
-      <h2 className={css.title}>{title}</h2>
-      <div className={css.timerPanel}>
-        <span>-</span>
-        <p>
-          <span className={css.value}> {date.day}</span>
-          <span className={css.units}>дн</span>
-        </p>
-        <span>:</span>
-        <p>
-          <span className={css.value}>{zeroPad(date.hour)}</span>
-          <span className={css.units}>год</span>
-        </p>
-        <span>:</span>
-        <p>
-          <span className={css.value}>{zeroPad(date.min)}</span>
-          <span className={css.units}>хв</span>
-        </p>
-        <span>:</span>
-        <p>
-          <span className={css.value}>{zeroPad(date.sec)}</span>
-          <span className={css.units}>сек</span>
-        </p>
+    timeEndState && (
+      <div className={css.container}>
+        <div className={css.timerPanel}>
+          <span>-</span>
+          <p>
+            <span className={css.value}> {date.day}</span>
+            <span className={css.units}>дн</span>
+          </p>
+          <span>:</span>
+          <p>
+            <span className={css.value}>{zeroPad(date.hour)}</span>
+            <span className={css.units}>год</span>
+          </p>
+          <span>:</span>
+          <p>
+            <span className={css.value}>{zeroPad(date.min)}</span>
+            <span className={css.units}>хв</span>
+          </p>
+          <span>:</span>
+          <p>
+            <span className={css.value}>{zeroPad(date.sec)}</span>
+            <span className={css.units}>сек</span>
+          </p>
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
 Clock.propTypes = {
-  title: PropTypes.string.isRequired,
-  dateOfEndISO: PropTypes.func.isRequired,
   finished: PropTypes.bool,
 };
 
