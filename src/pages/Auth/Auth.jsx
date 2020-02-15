bf792c4e5f8f78250e9c8d187f6836aeb47b0ccd
Lena <2863040@ukr.net>
2020-02-15 14:52:03 +0200

    fix css and add start page

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -8,7 +8,7 @@ import styles from './Auth.module.css';
 import Quote from '../../components/Quote/Quote';
 import QuoteRegistration from '../../components/QuoteRegistration/QuoteRegistration';
 
-function Auth(props) {
+const Auth = (props) => {
   const { pathname } = useLocation();
 
   return (
