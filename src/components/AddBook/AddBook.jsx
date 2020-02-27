023adbbbcfb52e255382b092a971154f6d6ccb5e
HardRye <nikolaimihail@gmail.com>
2020-02-28 00:53:44 +0200

    lib input notyfs

--- src/components/AddBook/AddBook.jsx
+++ src/components/AddBook/AddBook.jsx
@@ -3,13 +3,13 @@ import React, { useState } from 'react';
 import DateFnsUtils from '@date-io/date-fns';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
-import { Notyf } from 'notyf';
+import { ToastContainer, toast } from 'react-toastify';
 import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { useSelector, useDispatch } from 'react-redux';
 import { postBook } from '../../redux/books/BooksOperations';
 import { getUserToken } from '../../redux/selectors/sessionSelectors';
 import styles from './AddBook.module.css';
-import 'notyf/notyf.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 const AddBook = () => {
   const token = useSelector(state => getUserToken(state));
@@ -54,87 +54,131 @@ const AddBook = () => {
     },
   });
 
+  const [toastId, setToastId] = useState('');
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    if (name === 'bookName' && !value.match(/^(?! )(?!-).*$/)) {
+      setToastId(name);
+      toast('НЕ МОЖЕ ПОЧИНАТИСЯ З ПРОБІЛУ/ДЕФІСУ', {
+        type: 'error',
+        toastId,
+      });
+    }
+    if (name === 'bookAuthor' && !value.match(/^(?! )(?!-)(?!(?:.*\d)).*$/)) {
+      setToastId(name);
+      toast('НЕ МОЖЕ ПОЧИНАТИСЯ З ПРОБІЛУ/ДЕФІСУ ТА МІСТИТИ ЦИФРИ', {
+        type: 'error',
+        toastId,
+      });
+    }
+    if (name === 'pagesAmount' && value.length > 4) {
+      setToastId(name);
+      toast('ЗАНАДТО БАГАТО СТОРІНОК', {
+        type: 'error',
+        toastId,
+      });
+    }
+
+    formik.handleChange(e);
+  };
+
   return (
-    <form className={styles.addBookForm} onSubmit={formik.handleSubmit}>
-      <label htmlFor="bookName" className={styles.labelTitle}>
-        <div className={styles.inputTitle}>Назва книги</div>
-        <input
-          type="text"
-          name="bookName"
-          value={formik.values.bookName}
-          id="bookName"
-          placeholder="..."
-          className={styles.inputData}
-          onChange={formik.handleChange}
-          required
-        />
-        {formik.touched.bookName && formik.errors.bookName ? (
-          <span className={styles.bookNameError}>{formik.errors.bookName}</span>
-        ) : null}
-      </label>
-      <div className={styles.tabletWrapper}>
-        <label htmlFor="bookAuthor" className={styles.labelAutor}>
-          <div className={styles.inputTitle}>Автор книги</div>
+    <>
+      <form className={styles.addBookForm} onSubmit={formik.handleSubmit}>
+        <label htmlFor="bookName" className={styles.labelTitle}>
+          <div className={styles.inputTitle}>Назва книги</div>
           <input
             type="text"
-            name="bookAuthor"
-            value={formik.values.bookAuthor}
-            id="bookAuthor"
+            name="bookName"
+            value={formik.values.bookName}
+            id="bookName"
             placeholder="..."
             className={styles.inputData}
-            onChange={formik.handleChange}
+            onChange={handleChange}
+            onBlur={formik.handleBlur}
             required
           />
-          {formik.touched.bookAuthor && formik.errors.bookAuthor ? (
-            <span className={styles.bookAuthorError}>
-              {formik.errors.bookAuthor}
+          {formik.touched.bookName && formik.errors.bookName ? (
+            <span className={styles.bookNameError}>
+              {formik.errors.bookName}
             </span>
           ) : null}
         </label>
-        <label htmlFor="bookDate" className={styles.labelYear}>
-          <div className={styles.inputTitle}>Рік випуску</div>
-          <MuiPickersUtilsProvider utils={DateFnsUtils} id="bookDate">
-            <DatePicker
-              value={formik.values.bookDate}
-              onChange={date => formik.setFieldValue('bookDate', date)}
+        <div className={styles.tabletWrapper}>
+          <label htmlFor="bookAuthor" className={styles.labelAutor}>
+            <div className={styles.inputTitle}>Автор книги</div>
+            <input
+              type="text"
+              name="bookAuthor"
+              value={formik.values.bookAuthor}
+              id="bookAuthor"
+              placeholder="..."
               className={styles.inputData}
-              InputProps={{ className: styles.inputData }}
-              views={['year']}
-              invalidDateMessage=""
-              disableFuture
+              onChange={handleChange}
+              onBlur={formik.handleBlur}
+              required
             />
-          </MuiPickersUtilsProvider>
-        </label>
+            {formik.touched.bookAuthor && formik.errors.bookAuthor ? (
+              <span className={styles.bookAuthorError}>
+                {formik.errors.bookAuthor}
+              </span>
+            ) : null}
+          </label>
+          <label htmlFor="bookDate" className={styles.labelYear}>
+            <div className={styles.inputTitle}>Рік випуску</div>
+            <MuiPickersUtilsProvider utils={DateFnsUtils} id="bookDate">
+              <DatePicker
+                value={formik.values.bookDate}
+                onChange={date => formik.setFieldValue('bookDate', date)}
+                className={styles.inputData}
+                InputProps={{ className: styles.inputData }}
+                views={['year']}
+                invalidDateMessage=""
+                disableFuture
+                autoOk
+              />
+            </MuiPickersUtilsProvider>
+          </label>
 
-        <label htmlFor="pagesAmount" className={styles.labelPages}>
-          <div className={styles.inputTitle}>Кількість сторінок</div>
+          <label htmlFor="pagesAmount" className={styles.labelPages}>
+            <div className={styles.inputTitle}>Кількість сторінок</div>
+            <input
+              type="number"
+              name="pagesAmount"
+              value={formik.values.pagesAmount}
+              id="pagesAmount"
+              placeholder="..."
+              className={styles.inputData}
+              onChange={handleChange}
+              onBlur={formik.handleBlur}
+              min="0"
+              required
+            />
+            {formik.touched.pagesAmount && formik.errors.pagesAmount ? (
+              <span className={styles.pagesAmountError}>
+                {formik.errors.pagesAmount}
+              </span>
+            ) : null}
+          </label>
+        </div>
+        <label htmlFor="addBtn">
           <input
-            type="number"
-            name="pagesAmount"
-            value={formik.values.pagesAmount}
-            id="pagesAmount"
-            placeholder="..."
-            className={styles.inputData}
-            onChange={formik.handleChange}
-            min="0"
-            required
+            type="submit"
+            value="Додати"
+            id="addBtn"
+            className={styles.addBtn}
           />
-          {formik.touched.pagesAmount && formik.errors.pagesAmount ? (
-            <span className={styles.pagesAmountError}>
-              {formik.errors.pagesAmount}
-            </span>
-          ) : null}
         </label>
-      </div>
-      <label htmlFor="addBtn">
-        <input
-          type="submit"
-          value="Додати"
-          id="addBtn"
-          className={styles.addBtn}
-        />
-      </label>
-    </form>
+      </form>
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar
+        closeOnClick
+        pauseOnHover={false}
+      />
+    </>
   );
 };
 
