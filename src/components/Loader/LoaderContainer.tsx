8ddbbbd105bd9da706a276a5ff3b1a542d2633c1
StanislavKaisin <8050361@gmail.com>
2020-03-09 20:51:26 +0200

    changed Loader component

--- src/components/Loader/LoaderContainer.tsx
+++ src/components/Loader/LoaderContainer.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import Loader from './Loader';
 
-const LoaderContainer = () => {
-  const isLoader = useSelector(state => state.loader);
+// interfaces
 
+interface State {
+  loader?: boolean;
+}
+
+const LoaderContainer: React.FC | any = () => {
+  const isLoader = useSelector((state: State) => state.loader);
   return isLoader === true && <Loader />;
 };
 
